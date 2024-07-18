62ccca491fcc6782b24d3302c5d06901c994e739
Aastik-Mehta-22 <mehtaaastik12@gmail.com>
2024-07-19 02:20:35 +0530

    Question largest of 3 numbers done

---
+++ conditionsAndLoops/QLargest.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+public class QLargest {
+    public static void main(String[] args) {
+        // print largest of the 3 numbers
+        Scanner cin = new Scanner(System.in);
+        int num1 = cin.nextInt();
+        int num2 = cin.nextInt();
+        int num3 = cin.nextInt();
+
+        // if(num1 >= num2 && num1 >= num3){
+        //     System.out.println(num1);
+        // }
+        // else if(num2 >= num1 && num2 >= num3){
+        //     System.out.println(num2);
+        // }
+        // else{
+        //     System.out.println(num3);
+        // }
+
+        // int max = Math.max(num1, Math.max(num2, num3));
+        // System.out.println(max);
+        
+    }
+}
