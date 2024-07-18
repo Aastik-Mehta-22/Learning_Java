6c4a2cab9cd74a2150bf93d5062044aa1a2e5914
Aastik-Mehta-22 <mehtaaastik12@gmail.com>
2024-07-19 01:01:58 +0530

    basics added

---
+++ first-tutorial/Basics.java
@@ -0,0 +1,29 @@
+public class Basics {
+    public static void main(String[] args) {
+        // if (true) {
+        //     System.out.println("Hello World");
+        // }
+
+        // int a = 10;
+        // if (a == 10) {
+        //     System.out.println("Hi !");
+        // }
+
+        // if (true) System.out.println("Heey");
+
+        // if (true)
+        //     System.out.println("ok");
+
+
+        //first 5 numbers
+        // int count = 0;
+        // while (count != 5) {
+        //     System.out.println(++count);
+        // }
+
+        for(int count = 1;count<=5;count++){
+            System.out.println(count);
+        }
+
+    }
+}
