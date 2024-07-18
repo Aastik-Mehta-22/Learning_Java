b374a38046701df225ad049d37138e41a2c6ec28
Aastik-Mehta-22 <mehtaaastik12@gmail.com>
2024-07-19 02:04:53 +0530

    conditions and loops added

---
+++ conditionsAndLoops/Loops.java
@@ -0,0 +1,38 @@
+
+import java.util.Scanner;
+
+
+
+public class Loops {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+     
+
+        // print no from 1 to 5
+        // for(int i = 1;i<6;i++){
+        //     System.out.println(i);
+        // }
+
+        //print numbers from 1 to n
+        // int n  = in.nextInt();
+        // for(int i = 1;i<=n;i++){
+        //     System.out.print(i + " ");
+        // }
+
+        // int i = 1;
+        // while (i <= 100) {
+        //     System.out.println(i++);
+        // }
+
+        /*
+            Do while loop
+         */
+        
+        //  int i = 1;
+        //  do { it will always execute once
+        //     System.out.print(i+" ");
+        //     i++;
+        //  } while (i <= 100);
+    }   
+}
