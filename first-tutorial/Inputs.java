4981e5e9aa7e62cd32d0adcb741c160ceebd7633
Aastik-Mehta-22 <mehtaaastik12@gmail.com>
2024-07-18 23:48:48 +0530

    inputs added

---
+++ first-tutorial/Inputs.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Inputs {
+    public static void main(String[] args) {
+        System.out.print("Please enter some input : ");
+        Scanner input = new Scanner(System.in);
+        // int rollno = input.nextInt();
+        // System.out.println("Your roll no is " + rollno);
+
+        // int a = 234_000_000;
+        // System.out.println(a);
+
+        // String name = input.nextLine(); // we want all line as an input
+        // System.out.println(name);
+
+        // String namesecond = input.next(); // i wtill take only first word as input
+        // System.out.println(namesecond);
+
+        // float marks = input.nextFloat();
+        // System.out.println(marks);
+        
+
+        
+
+    }
+}
