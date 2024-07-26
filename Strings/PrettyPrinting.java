174b98f17265549c2e054f8897a0088fb196c4ee
Aastik-Mehta-22 <mehtaaastik12@gmail.com>
2024-07-26 15:02:09 +0530

    preety printing

---
+++ Strings/PrettyPrinting.java
@@ -0,0 +1,9 @@
+public class PrettyPrinting {
+    public static void main(String[] args) {
+        float a = 453.1274f;
+        System.out.printf("Formatted number is %.2f",a);
+        System.out.println();
+        System.out.printf("%.3f\n",Math.PI);
+        System.out.printf("My name is %s and I am %s","aastik","cool");
+    }
+}
