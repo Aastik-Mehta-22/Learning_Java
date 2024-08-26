c0480026f43376e5d979085233a00fa467fcf83d
Aastik-Mehta-22 <mehtaaastik12@gmail.com>
2024-08-26 18:12:36 +0530

    Breadth first search in binary tree

---
+++ Trees/Questions/BFS.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class  BFS{
+
+    
+    public static void main(String[] args) {
+        
+        BinaryTree tree = new BinaryTree();
+        Scanner sc =  new Scanner(System.in);
+        ArrayList<Integer> bfs = new ArrayList<>();
+
+        tree.populate(sc);
+        tree.display();
+        BFS(tree,bfs);
+
+
+    }
+
+    public void BFS(BinaryTree tree,ArrayList<Integer> bfs){
+        Queue
+    }
+}
